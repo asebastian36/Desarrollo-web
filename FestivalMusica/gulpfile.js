0d4cff51f67a22e7c545ca668016046e7873e6ea
asebastian36 <asebastianjob@gmail.com>
2024-02-12 19:18:18 -0600

    avances de la seccion del festival de musica

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,11 +1,13 @@
 //  extraer funcionalidades de las dependencias
 const { src, dest, watch } = require('gulp');
 const sass =  require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 //  creando una tarea
 function css(done) {
     //  identificar el sass, compilarlo y almacenarlo
     src('src/scss/**/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(dest('build/css'));
 
